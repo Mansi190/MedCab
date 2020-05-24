ff58851379029be2e28b0cf278f37f71c2a4fd75
Mansi <mansi.manjrekar190>
2020-05-24 11:57:24 +0530

    Ambulance registration done...firebase retrival

--- app/src/main/java/com/example/ambcab/Select_user.java
+++ app/src/main/java/com/example/ambcab/Select_user.java
@@ -7,26 +7,52 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class Select_user extends AppCompatActivity {
-    Button Patient_button;
-    private  View.OnClickListener PatientOnClickListner=new View.OnClickListener() {
+public class Select_user extends AppCompatActivity
+{
+    Button Patient_button, Ambulance_button;
+    private View.OnClickListener PatientOnClickListner = new View.OnClickListener()
+    {
         @Override
-        public void onClick(View v) {
+        public void onClick(View v)
+        {
             Patient();
         }
     };
+    private View.OnClickListener AmbulanceOnClickListner = new View.OnClickListener()
+    {
+        @Override
+        public void onClick(View v) {
+            Ambulance();
+        }
 
-    private void Patient() {
-        Intent i = new Intent(this,PersonalDetails.class);
-        startActivity(i);
-        overridePendingTransition(R.anim.slideinright,R.anim.slideoutleft);
-    }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_user);
-        Patient_button=findViewById(R.id.button2);
+
+        Patient_button = findViewById(R.id.button2);
+        Ambulance_button = findViewById(R.id.button3);
+
         Patient_button.setOnClickListener(PatientOnClickListner);
+        Ambulance_button.setOnClickListener(AmbulanceOnClickListner);
     }
+
+    private void Patient() {
+        Intent i = new Intent(this, PersonalDetails.class);
+        startActivity(i);
+        overridePendingTransition(R.anim.slideinright, R.anim.slideoutleft);
+    }
+
+    private void Ambulance() {
+        Intent intent = new Intent(this, ambulanceRegistration.class);
+        startActivity(intent);
+        overridePendingTransition(R.anim.slideinright, R.anim.slideoutleft);
+    }
+
 }
+
+
+
